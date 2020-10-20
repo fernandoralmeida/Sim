b88128720aadbf3af54bbfc2d1e3d843c6c593d8
Fernando Almeida <fernando.ralmeida@outlook.com>
2020-10-20 11:40:25 -0300

    up-05

--- Administracao/Model/RepositorioSetores.cs
+++ Administracao/Model/RepositorioSetores.cs
@@ -43,5 +43,69 @@ namespace Sim.Modulos.Administracao.Model
 
             return lst;
         }
+        /// <summary>
+        /// Adiciona novo setor no banco de dados.
+        /// </summary>
+        /// <param name="obj">grava o obj setores no banco</param>
+        /// <returns></returns>
+        public bool Gravar(Setores obj)
+        {
+            try
+            {
+                var dataAccess = Instances.DataBase5();
+
+                dataAccess.ClearParameters();
+
+                dataAccess.AddParameters("@Setor", obj.Setor);
+                dataAccess.AddParameters("@Secretaria", obj.Secretaria);
+                dataAccess.AddParameters("@Ativo", obj.Ativo);
+
+                return dataAccess.Write(@"INSERT INTO Setores (Setor, Secretaria, Ativo) VALUES (@Setor, @Secretaria, @Ativo)");
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Retorna todos os setores
+        /// </summary>
+        /// <param name="secretaria">retorna somente setores por secretaria</param>        /// 
+        /// <returns></returns>
+        public ObservableCollection<Setores> Setores(string secretaria)
+        {
+            var dataAccess = Instances.DataBase5();
+
+            ObservableCollection<Setores> lst = new ObservableCollection<Setores>();
+
+            try
+            {
+                dataAccess.ClearParameters();
+                dataAccess.AddParameters("@Sec", "%" + secretaria + "%");
+
+                foreach (DataRow dr in dataAccess.Read(@"SELECT * FROM Setores WHERE (Secretaria LIKE @Sec) AND (Ativo = true) ORDER BY Setor").Rows)
+                {
+                    var setor = new Setores
+                    {
+                        Indice = (int)dr["Indice"],
+                        Setor = (string)dr["Setor"],
+                        Secretaria = (string)dr["Secretaria"],
+                        Ativo = (bool)dr["Ativo"]
+                    };
+                    lst.Add(setor);
+                }
+                return lst;
+            }
+            catch(Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+                return null;
+            }
+
+            
+        }
     }
 }
--- Administracao/ViewModel/OpcoesV/Setores.cs
+++ Administracao/ViewModel/OpcoesV/Setores.cs
@@ -2,10 +2,107 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
 
 namespace Sim.Modulos.Administracao.ViewModel.OpcoesV
 {
-    class Setores
+    using Sim.Mvvm.Helpers.Notifiers;
+    using Sim.Mvvm.Helpers.Observers;
+    using Model;
+    using Sim.Mvvm.Helpers.Commands;
+
+    public class Setores : NotifyProperty
     {
+        #region Declaracoes
+        ObservableCollection<Model.Setores> _setores = new ObservableCollection<Model.Setores>();
+        Model.Setores _setor = new Model.Setores();
+        Visibility _insetON = Visibility.Collapsed;
+        #endregion
+
+        #region Propriedades
+        public ObservableCollection<Model.Setores> ListaSetores
+        {
+            get { return _setores; }
+            set { _setores = value; RaisePropertyChanged("ListaSetores"); }
+        }
+
+        public Model.Setores Setor
+        {
+            get { return _setor; }
+            set { _setor = value; RaisePropertyChanged("Setor"); }
+        }
+
+        public Visibility InsetON
+        {
+            get { return _insetON; }
+            set { _insetON = value; RaisePropertyChanged("InsetON"); }
+        }
+        #endregion
+
+        #region Comandos
+        public ICommand CommandRefresh => new RelayCommand(p => {
+            if (Setor.Secretaria == string.Empty || Setor.Secretaria == null || Setor.Secretaria == "...")
+                Listar("%");
+            else
+                Listar(Setor.Secretaria);
+        });
+
+        public ICommand CommandShow => new RelayCommand(p => {
+            InsetON = Visibility.Visible;
+        });
+
+        public ICommand CommandSave => new RelayCommand(p => {
+
+            Setor.Ativo = true;
+            if (Gravar().IsCompleted == true)
+            {
+                InsetON = Visibility.Collapsed;
+                Listar(Setor.Secretaria);
+            }
+        });
+
+        public ICommand CommandCancelar => new RelayCommand(p => {
+            InsetON = Visibility.Collapsed;
+        });
+        #endregion
+
+        #region Construtor
+        public Setores()
+        {            
+            InsetON = Visibility.Collapsed;
+            Listar("%");
+        }
+        #endregion
+
+        #region Metodos e Funcoes
+        /// <summary>
+        /// Task que retorna lista dos setores
+        /// </summary>
+        /// <param name="_param">filtra setores por nome de secretaria</param>
+        /// <returns></returns>
+        private Task Listar(string _param)
+        {
+            var t = Task<ObservableCollection<Model.Setores>>.Factory.StartNew(() => {
+                return ListaSetores = new RepositorioSetores().Setores(_param);
+            });
+            t.Wait();
+            return t;
+        }
+
+        private Task Gravar()
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                new RepositorioSetores().Gravar(Setor);
+            });
+            t.Wait();
+            return t;
+        }
+        #endregion
+
+
     }
 }
